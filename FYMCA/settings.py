426a0c17c053aeff2638a961f97f2bfd6e8c2c59
Siddhi Shirke <60963725+SiddhiShirke@users.noreply.github.com>
2023-10-16 22:01:53 +0530

    Update settings.py

--- FYMCA/settings.py
+++ FYMCA/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'siddhisshirke01@gmail.com'
-EMAIL_HOST_PASSWORD = 'cwcujsprolnwlivj'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "placement.signup"
